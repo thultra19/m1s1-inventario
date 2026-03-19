b81cd49457adfdab99122b3927669553a993dc20
thultra19 <thultra19@gmail.com>
2026-03-19 15:18:59 -0500

    Add calcular_valor_inventario function for inventory value

--- INVfunciones.py
+++ INVfunciones.py
@@ -4,3 +4,15 @@ def registrar_productos (nombre_producto, cantidad_producto, precios_producto):
     print(f"Producto: {nombre_producto} | Precio: {precios_producto} | Cantidad: {cantidad_producto} | Total: {total_producto}")
     print ("el producto a sido registrado")
     return total_producto
+
+
+def calcular_valor_inventario(inventario):
+
+    valor_total = 0
+    cantidad_total = 0
+
+    for producto in inventario:
+        cantidad_total += producto["Cantidad"]
+        valor_total += producto["Total"]
+
+    return valor_total, cantidad_total
